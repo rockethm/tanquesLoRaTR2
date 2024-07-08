a64e6b0a5a4a7032c7b7537b2989efe67a5a3a44
Andre Melo <andredeoliveiramelo@gmail.com>
2024-07-08 17:47:20 -0300

    Commit

--- LoRa_Rec_Ultrassom/LoRa_Rec_Ultrassom/config.h
+++ LoRa_Rec_Ultrassom/LoRa_Rec_Ultrassom/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME "rockethm_"
-#define IO_KEY "aio_EcTe78ypmyuuMC4miZ2KxFjSNHHr"
+#define IO_USERNAME "insiraUmUsuarioAdafruitAqui"
+#define IO_KEY "insiraUmaChaveAdafruitAqui"
 
 /******************************* WIFI **************************************/
 
@@ -20,8 +20,8 @@
 //   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
 //   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
 
-#define WIFI_SSID "Andre"
-#define WIFI_PASS "plk330qw"
+#define WIFI_SSID "nomeDaRedeWifi"
+#define WIFI_PASS "senhaDaRedeWifi"
 
 // uncomment the following line if you are using airlift
 // #define USE_AIRLIFT
